a4f5c8f1674aac08890826c2284d5d5aad268a3c
Alexandre <xand974@gmail.com>
2021-09-10 21:25:37 +0200

    ajout nav

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,13 @@
+import Navbar from "components/Navbar";
 import styled from "styled-components";
 
 const Container = styled.div`
   height: 100vh;
-  background: blue;
 `;
 const App = () => {
   return (
     <Container>
-      <p>ca marche</p>
+      <Navbar />
     </Container>
   );
 };
--- front/src/components/Navbar.jsx
+++ front/src/components/Navbar.jsx
@@ -1,10 +1,67 @@
 import styled from "styled-components";
-const Nav = styled.div``;
+const Nav = styled.div`
+  height: 50px;
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Left = styled.div`
+  width: 60%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+  text-decoration: underline #5a0413;
+  font-size: 30px;
+`;
+
+const Menu = styled.ul`
+  display: flex;
+  align-items: center;
+`;
+const Item = styled.li`
+  margin: 0px 10px;
+  list-style-type: none;
+  font-weight: bold;
+  font-size: 20px;
+  color: gray;
+`;
+
+const Button = styled.button`
+  border: 3px solid black;
+  padding: 10px 20px;
+  font-size: 17px;
+  background-color: gray;
+  color: white;
+  font-weight: bold;
+  border-radius: 10px;
+  cursor: pointer;
+`;
 
 export default function Navbar() {
   return (
     <Nav>
-      <nav></nav>
+      <Wrapper>
+        <Left>
+          <Logo>Into the Dipe</Logo>
+          <Menu>
+            <Item>Acceuil</Item>
+            <Item>Fonctionnalités</Item>
+            <Item>Services</Item>
+            <Item>Contact</Item>
+            <Item>Prix</Item>
+          </Menu>
+        </Left>
+        <Button>Join now</Button>
+      </Wrapper>
     </Nav>
   );
 }
