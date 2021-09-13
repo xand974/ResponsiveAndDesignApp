85149a2803b3b5672001e3e86a1e6a10e5d2c77e
Alexandre <xand974@gmail.com>
2021-09-14 00:41:30 +0200

    ajout feature section

--- front/src/App.js
+++ front/src/App.js
@@ -1,22 +1,32 @@
 import Feature from "components/Feature";
 import Intro from "components/Intro";
 import Navbar from "components/Navbar";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const Container = styled.div`
   height: 100vh;
   overflow: hidden;
   position: relative;
 `;
-const Shape = styled.div`
-  background: #0d3f66;
-  clip-path: polygon(78% 0, 100% 0, 100% 100%, 56% 100%);
+const IntoShape = css`
   z-index: -1;
   position: absolute;
   width: 100%;
   height: 100%;
   top: 0;
   left: 0;
+`;
+
+const Shape = styled.div`
+  ${IntoShape}
+  background: #0d3f66;
+  clip-path: polygon(78% 0, 100% 0, 100% 100%, 56% 100%);
+`;
+
+const FeatureShape = styled.div`
+  ${IntoShape}
+  background: #ffa8b8;
+  clip-path: polygon(0 0, 56% 0%, 100% 100%, 56% 100%);
 `;
 
 const App = () => {
@@ -28,6 +38,7 @@ const App = () => {
         <Intro />
       </Container>
       <Container>
+        <FeatureShape />
         <Feature />
       </Container>
     </>
--- front/src/components/Feature.jsx
+++ front/src/components/Feature.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 import phone from "../images/phone.png";
+import AnimatedShapes from "./AnimatedShapes";
 const Container = styled.div`
   display: flex;
   height: 100vh;
+  align-items: center;
 `;
 
 const Left = styled.div`
@@ -12,6 +14,9 @@ const Left = styled.div`
 
 const Right = styled.div`
   width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 `;
 
 const Image = styled.img`
@@ -19,10 +24,32 @@ const Image = styled.img`
   object-fit: cover;
 `;
 
-const Title = styled.span``;
-const Subtitle = styled.span``;
-const Description = styled.p``;
-const Button = styled.button``;
+const Title = styled.span`
+  font-size: 70px;
+`;
+const Subtitle = styled.span`
+  font-size: 25px;
+  font-style: italic;
+  color: #383838;
+  margin-top: 30px;
+`;
+const Description = styled.p`
+  font-size: 20px;
+  margin-top: 30px;
+  color: #565656;
+`;
+const Button = styled.button`
+  width: 190px;
+  padding: 15px 25px;
+  background-color: #0000ff;
+  margin-top: 30px;
+  border: none;
+  color: white;
+  font-size: 17px;
+  border-radius: 15px;
+  font-weight: bold;
+  cursor: pointer;
+`;
 
 export default function Feature() {
   return (
@@ -31,11 +58,22 @@ export default function Feature() {
         <Image src={phone} />
       </Left>
       <Right>
-        <Title></Title>
-        <Subtitle></Subtitle>
-        <Description></Description>
-        <Button></Button>
+        <Title>
+          <b>Real time</b> chat app
+        </Title>
+        <Title>
+          <b>Ready</b> to use
+        </Title>
+        <Subtitle>
+          Etends ton cercle d'amis et rencontre des personnes du monde entier.{" "}
+        </Subtitle>
+        <Description>
+          Nous avons crée un système de rencontre innovante et nous avons
+          l'intention de vous la faire profiter{" "}
+        </Description>
+        <Button>COMMENCER</Button>
       </Right>
+      <AnimatedShapes />
     </Container>
   );
 }
