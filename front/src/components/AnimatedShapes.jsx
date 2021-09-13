8a7a4752cbbd5c8dc3fd0b76007af271cfaa259c
Alexandre <xand974@gmail.com>
2021-09-13 11:42:32 +0200

    anim

--- front/src/components/AnimatedShapes.jsx
+++ front/src/components/AnimatedShapes.jsx
@@ -26,11 +26,15 @@ const Square = styled.div`
 const Circle = styled.div`
   height: 100px;
   width: 100px;
+  background-color: #03030f;
   border-radius: 100%;
+  opacity: 0.7;
   position: absolute;
-  top: 0;
+  z-index: -1;
+  top: 200px;
   left: 0;
-  animation: circle 25s linear infinite alternate;
+  animation: circle 40s linear infinite alternate;
+  box-shadow: 0 0 10px white;
 
   @keyframes circle {
     0%,
@@ -47,18 +51,21 @@ const Rectangle = styled.div`
   height: 100px;
   width: 200px;
   position: absolute;
-  top: 0;
+  background-color: #eaeaff;
+  opacity: 0.7;
+  top: 100px;
   z-index: -1;
   left: 0;
-  animation: rectangle 25s linear infinite alternate;
+  animation: rectangle 50s linear infinite alternate;
+  box-shadow: 0 0 10px white;
 
   @keyframes rectangle {
     0%,
     100% {
-      transform: translate(0, 0);
+      transform: translate(0, 100vh);
     }
     50% {
-      transform: translate(-100vw, -100vh);
+      transform: translate(100vw, 0);
     }
   }
 `;
